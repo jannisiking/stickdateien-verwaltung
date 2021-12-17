5d8db799e55ae41e2f29348fa4c7acd3b63b911e
DESKTOP-9EQUUEP\janni <jannis.iking@gmail.com>
2021-12-17 17:11:14 +0100

    MongoDb Inserting and Reading ID in the Making

--- pages/api/storeData.js
+++ pages/api/storeData.js
@@ -1,8 +1,22 @@
 import fs from "fs";
 import path from "path";
+const { MongoClient } = require("mongodb");
 const formidable = require("formidable");
 
+// Connection URL
+const url = "mongodb://homeserver:32785";
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = "stickdateien_verwaltung";
+
 export default async function handler(req, res) {
+  await client.connect();
+  const db = client.db(dbName);
+  const collection = db.collection("stickdateien");
+//   const temp = await collection.find().sort({_id:-1}).limit(1).toArray();
+// const maxid = temp[0]._id.toHexString();
+//   console.log(maxid);
   const id = Math.round(Math.random() * 1000);
   const dir = path.join(__dirname, `../../../../filestorage/${id}`);
   fs.mkdirSync(dir);
@@ -23,6 +37,10 @@ export default async function handler(req, res) {
       console.log(err);
       res.status(500).send();
     } else {
+      collection.insertOne({_id: {
+                              $oid:"000000000000000000000001"
+                            },
+                          name: "Schalke"});
       res.status(200).send();
     }
   });
